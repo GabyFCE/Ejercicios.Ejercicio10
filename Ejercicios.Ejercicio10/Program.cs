60af27b079342c1490f2c0b1c5926c9c65f2be20
GabyFCE <duartegabriel2017@gmail.com>
2023-04-02 06:02:29 -0300

    Add project files.

---
+++ Ejercicios.Ejercicio10/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Ingrese una frase que contenga minúsculas.");
+string frase = Console.ReadLine();
+Console.WriteLine("La frase con todas las letras en mayusculas es: " + frase.ToUpper());
